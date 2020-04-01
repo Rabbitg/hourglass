eb6472f8f50d77fa8a405b17cc0c1571796cef1f
Rabbitg <fi8608@naver.com>
2020-04-01 16:12:01 +0900

    Image upload

--- app/src/main/java/com/timeattack/hourglass/Login/RegisterActivity.kt
+++ app/src/main/java/com/timeattack/hourglass/Login/RegisterActivity.kt
@@ -1,13 +1,21 @@
 package com.timeattack.hourglass.Login
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import com.timeattack.hourglass.R
 import kotlinx.android.synthetic.main.activity_register.*
+import java.util.*
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -20,55 +28,132 @@ class RegisterActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
 
-        // 회원가입 나열되어 있는 것
-        val name = findViewById<EditText>(R.id.nameRegisterEtv)
-        val email = findViewById<EditText>(R.id.emailRegisterEtv)
-        val nickname = findViewById<EditText>(R.id.nickNameRegisterEtv)
-        val password = findViewById<EditText>(R.id.pwRegisterEtv)
-        val passwordCheck = findViewById<EditText>(R.id.pwCheckRegisterEtv)
-
+        //회원가입 버튼
         registerButton.setOnClickListener {
-            if(email.text.toString().isEmpty() || password.text.toString().isEmpty() ||
-                    name.text.toString().isEmpty() || nickname.text.toString().isEmpty() || passwordCheck.text.toString().isEmpty())
-            {
-                Toast.makeText(this, "번거로우시겠지만 모두 작성해주세요 ^^", Toast.LENGTH_SHORT).show()
-            } else if (password.text.toString().length < 6)
-            {
-                Toast.makeText(this, "비밀번호는 7자리 이상이어야 합니다.", Toast.LENGTH_SHORT).show()
-            }
-            else if (password.text.toString() != passwordCheck.text.toString())
-            {
-                Toast.makeText(this, "두 비밀번호가 일치하여야 합니다.", Toast.LENGTH_SHORT).show()
-            }
-            else{
-                //여기서 막힌듯
-                auth.createUserWithEmailAndPassword(email.text.toString(),password.text.toString())
-                    .addOnCompleteListener(this){task ->
-                        if (task.isSuccessful){
-                            Log.d(TAG, "createUserWithEmail:success")
-                            val user = auth.currentUser
-                            //updateUI(user)
-                            // 아니면 액티비티를 닫아 버린다.
-                            finish()
-                        }else
-                        {
-                            Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                            Toast.makeText(
-                                baseContext, "Authentication failed.",
-                                Toast.LENGTH_SHORT
-                            ).show()
+            //회원 가입 입력 폼
+            performRegister()
+        }
+        selectPhoto_button.setOnClickListener {
+            Log.d(TAG, "Try to show photo selector")
 
-                        }
-                    }
-            }
-
-
-
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
         }
 
+
+        // 뒤로가기 버튼
         backbuttonRegister.setOnClickListener {
             super.onBackPressed()
         }
 
     }
+
+    var selectedPhotoUri: Uri? = null
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            // proceed and check what the selected image was....
+            Log.d(TAG, "Photo was selected")
+
+            selectedPhotoUri = data.data
+
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+
+            selectPhoto_button.setImageBitmap(bitmap)
+
+
+        }
+    }
+
+    private fun uploadImageToFirebaseStorage() {
+
+        if(selectedPhotoUri == null) return
+
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
+        ref.putFile(selectedPhotoUri!!)
+            .addOnSuccessListener {
+                Log.d("RegisterActivity","Successfully uploaded image: ${it.metadata?.path}")
+
+                ref.downloadUrl.addOnSuccessListener {
+                    Log.d("RegisterActivity","File Location: $it")
+                    saveUserToFirebaseDataBase(it.toString())
+                }
+            }
+            .addOnFailureListener {
+                // do some logging here
+            }
+    }
+
+    private fun saveUserToFirebaseDataBase(profileImageUrl: String) {
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+
+        val user = User(uid,nameRegisterEtv.text.toString(), profileImageUrl)
+        ref.setValue(user)
+            .addOnSuccessListener {
+                Log.d("RegisterActivity", "Finally we saved the user to Firebase Database")
+            }
+    }
+
+    // 회원가입 함수
+    private fun performRegister(){
+        // 회원가입 나열되어 있는 것
+        val name = findViewById<EditText>(R.id.nameRegisterEtv)
+        val email = emailRegisterEtv.text.toString()
+        val nickname = findViewById<EditText>(R.id.nickNameRegisterEtv)
+        val password = pwRegisterEtv.text.toString()
+        val passwordCheck = findViewById<EditText>(R.id.pwCheckRegisterEtv)
+
+        if(email.isEmpty() || password.isEmpty() ||
+            name.text.toString().isEmpty() || nickname.text.toString().isEmpty() || passwordCheck.text.toString().isEmpty())
+        {
+            Toast.makeText(this, "번거로우시겠지만 모두 작성해주세요 ^^", Toast.LENGTH_SHORT).show()
+        } else if (password.length < 6)
+        {
+            Toast.makeText(this, "비밀번호는 7자리 이상이어야 합니다.", Toast.LENGTH_SHORT).show()
+        }
+        else if (password != passwordCheck.text.toString())
+        {
+            Toast.makeText(this, "두 비밀번호가 일치하여야 합니다.", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            auth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this){task ->
+                    if (task.isSuccessful){
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        finish()
+                    }else
+                    {
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                    }
+                }
+        }
+
+        // Firebase Authentication to create a user with email and password
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+                // else if successful
+                Log.d(TAG, "Successfully created user with uid: ${it.result?.user?.uid}")
+
+                uploadImageToFirebaseStorage()
+            }
+            .addOnFailureListener{
+                Log.d(TAG, "Failed to create user: ${it.message}")
+                Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    class User(val uid:String, val username:String, val profileImageUrl:String)
+
 }
